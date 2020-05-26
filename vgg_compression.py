26c269b412ddaa13cc77949cbc2132e5fcf18415
cody <cjb92@txstate.edu>
2020-05-26 11:18:44 -0500

    fix training end

--- vgg_compression.py
+++ vgg_compression.py
@@ -30,8 +30,8 @@ VALIDATION_SIZE = 10000
 BATCH_SIZE_PER_GPU = 64
 global_batch_size = (BATCH_SIZE_PER_GPU * 1)
 NUM_CLASSES = 10
-EPOCHS = 64
 TEST = 1
+EPOCHS = 64 if TEST == 1 else 2
 
 #%% [markdown]
 # Dataset code
@@ -136,68 +136,68 @@ def load_image_test(datapoint):
   return input_image, tf.one_hot(label, depth=NUM_CLASSES)
 
 class LayerBatch(tf.keras.utils.Sequence):
-    
+
     def __init__(self, input_model, dataset):
         self.input_model = input_model
         self.dataset = dataset.__iter__()
-        
+
     def __len__(self):
         return math.ceil(TRAIN_SIZE // global_batch_size )
-    
+
     def __getitem__(self, index):
         X, y = self.input_model(next(self.dataset))
         return X, y
-    
+
 class LayerBatchSynth(tf.keras.utils.Sequence):
-    
+
     def __init__(self, input_model, dataset):
         self.input_model = input_model
         self.dataset = dataset.__iter__()
-        
+
     def __len__(self):
         return math.ceil(4224 // global_batch_size )
-    
+
     def __getitem__(self, index):
         X, y = self.input_model(next(self.dataset))
         return X, y
-    
+
 import math
 class LayerTest(tf.keras.utils.Sequence):
-    
+
     def __init__(self, input_model, dataset):
         self.input_model = input_model
         self.dataset = dataset.__iter__()
-        
+
     def __len__(self):
         return math.ceil(VALIDATION_SIZE // global_batch_size )
-    
+
     def __getitem__(self, index):
         X, y = self.input_model(next(self.dataset))
         return X, y
 
 def add_layers(inputs, filters, layers=2):
     print(inputs.get_shape())
-    X = tf.keras.layers.SeparableConv2D(name=f'sep_conv_{build_replacement.counter}', filters=filters, 
+    X = tf.keras.layers.SeparableConv2D(name=f'sep_conv_{build_replacement.counter}', filters=filters,
                                         kernel_size= (3,3),
                                         padding='Same')(inputs)
     #X = tf.keras.layers.BatchNormalization(name=f'batch_norm_{build_replacement.counter}')(X)
     X = tf.keras.layers.ReLU(name=f'relu_{build_replacement.counter}')(X)
-    
+
     build_replacement.counter += 1
-    
+
     for i in range(1, layers):
         X = tf.keras.layers.SeparableConv2D(name=f'sep_conv_{build_replacement.counter}', filters=filters,
-                                            kernel_size=(3,3), 
+                                            kernel_size=(3,3),
                                             padding='Same')(X)
         #X = tf.keras.layers.BatchNormalization(name=f'batch_norm_{build_replacement.counter}')(X)
         X = tf.keras.layers.ReLU(name=f'relu_{build_replacement.counter}')(X)
         build_replacement.counter += 1
-    
+
     return X
-    
+
 def build_replacement(get_output, layers=2):
     inputs = tf.keras.Input(shape=get_output.output[0].shape[1::])
-    
+
     X = add_layers(inputs, get_output.output[1].shape[-1], layers)
     replacement_layers = tf.keras.Model(inputs=inputs, outputs=X)
     return replacement_layers
@@ -205,7 +205,7 @@ def build_replacement(get_output, layers=2):
 build_replacement.counter = 0
 
 def replac(inp, filters):
-    
+
     return add_layers(inp, filters,layers=2)
 
 def make_list(X):
@@ -259,7 +259,7 @@ def replace_layer(model, replace_layer_subname, replacement_fn,
             # I added the exception model_3_3/Identity:0 I think the problem is that is the input layer
             inpt = list_no_list([tsr_dict[name]  for name in inpt_names])
 
-            ### remake layer 
+            ### remake layer
             if layer.name in replace_layer_subname:
               if "relu" in layer.name or 'bn' in layer.name:
                 print('deleting ' + layer.name)
@@ -319,13 +319,13 @@ pprint.pprint(targets)
 
 for target in targets:
 
-    writer = tf.summary.create_file_writer(f"./summarys/{target['name']}")
+    writer = tf.summary.create_file_writer(f"./summarys/vgg/cifar10/{target['name']}")
     with writer.as_default():
         print(f"training layer {target['name']}")
         tf.keras.backend.clear_session()
         model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
         in_layer = target['layer']
-        get_output = tf.keras.Model(inputs=model.input, outputs=[model.layers[in_layer - 1].output, 
+        get_output = tf.keras.Model(inputs=model.input, outputs=[model.layers[in_layer - 1].output,
                                                                 model.layers[in_layer].output])
 
 
@@ -352,11 +352,11 @@ for target in targets:
             tf.keras.backend.clear_session()
             model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
             in_layer = target['layer']
-            get_output = tf.keras.Model(inputs=model.input, outputs=[model.layers[in_layer - 1].output, 
+            get_output = tf.keras.Model(inputs=model.input, outputs=[model.layers[in_layer - 1].output,
                                                                     model.layers[in_layer].output])
 
 
-            
+
             layer_train_gen = LayerBatch(get_output, train_dataset)
             layer_test_gen = LayerTest(get_output, test_dataset)
 
@@ -370,7 +370,7 @@ for target in targets:
                                         callbacks=[reduce_lr, early_stop],
                                         validation_steps=VALIDATION_SIZE // global_batch_size // TEST,
                                         verbose=1)
-            
+
             replacement_layers.save('/tmp/layer.h5')
 
             target['weights'] = [replacement_layers.layers[1].get_weights(), replacement_layers.layers[3].get_weights()]
@@ -399,59 +399,65 @@ for target in targets:
             # if np.abs(OG[1] - target['score'][1]) < 0.0001:
             #     print('stoping early')
             #     break
-    
 
 
-        
+
+
 
 tf.keras.backend.clear_session()
-model = tf.keras.models.load_model('./base_model_cifar10_vgg.h5')
+model = tf.keras.models.load_model('./base_model_cifar10_vgg16.h5')
 
 
-for target in targets[::-1]:
-    print(f'replacing layer {target["name"]}')
+writer = tf.summary.create_file_writer(f"./summarys/vgg/cifar10/final_model")
+with writer.as_default():
+    for target in targets[::-1]:
+        print(f'replacing layer {target["name"]}')
 
-    layer_name = target['name']
-    layer_pos = target['layer']
-    filters = model.layers[layer_pos].output.shape[-1]
+        layer_name = target['name']
+        layer_pos = target['layer']
+        filters = model.layers[layer_pos].output.shape[-1]
 
 
 
-    new_model = replace_layer(model, layer_name, lambda x: replac(x, filters))
-    new_model.layers[layer_pos].set_weights(target['weights'][0])
-    new_model.layers[layer_pos + 2].set_weights(target['weights'][1])
+        new_model = replace_layer(model, layer_name, lambda x: replac(x, filters))
+        new_model.layers[layer_pos].set_weights(target['weights'][0])
+        new_model.layers[layer_pos + 2].set_weights(target['weights'][1])
 
-    new_model.save('cifar10_vgg_modified.h5')
+        new_model.save('cifar10_vgg_modified.h5')
+        tf.keras.backend.clear_session()
+        model = tf.keras.models.load_model('cifar10_vgg_modified.h5')
+
     tf.keras.backend.clear_session()
     model = tf.keras.models.load_model('cifar10_vgg_modified.h5')
+    model.compile(optimizer=tf.keras.optimizers.SGD(.1), loss="categorical_crossentropy", metrics=['accuracy'])
+    final = model.evaluate(test_dataset, steps=VALIDATION_SIZE // global_batch_size // TEST)
 
-tf.keras.backend.clear_session()
-model = tf.keras.models.load_model('cifar10_vgg_modified.h5')
-model.compile(optimizer=tf.keras.optimizers.SGD(.1), loss="categorical_crossentropy", metrics=['accuracy'])
-model.evaluate(test_dataset, steps=VALIDATION_SIZE // global_batch_size // TEST)
+    tf.summary.scalar(name='model_acc', data=final[1], step=0)
+    tf.summary.scalar(name='model_loss', data=final[0], step=0)
 
+    writer.flush()
 
 
 #%%
 # for target in targets:
-    
+
 #     print(f'Replacing Layer {target["name"]}')
-    
+
 #     tf.keras.backend.clear_session()
-    
+
 #     model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
-    
+
 #     layer_name = target['name']
 #     layer_pos = target['layer']
 #     filters = model.layers[layer_pos].output.shape[-1]
-    
-    
+
+
 #     new_model = replace_layer(model, layer_name, lambda x: replac(x, filters))
 #     new_model.layers[layer_pos].set_weights(target['weights'][0])
 #     new_model.layers[layer_pos + 2].set_weights(target['weights'][1])
 #     new_model.compile(optimizer=tf.keras.optimizers.SGD(.1), loss="categorical_crossentropy", metrics=['accuracy'])
 #     target['score'] = new_model.evaluate(test_dataset, steps=VALIDATION_SIZE // global_batch_size)
-    
+
 
 
 
