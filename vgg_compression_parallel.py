f062222188350bb34c0137782a9fb98bbdce9916
cody <cjb92@txstate.edu>
2020-05-26 13:03:01 -0500

    fix growth

--- vgg_compression_parallel.py
+++ vgg_compression_parallel.py
@@ -13,7 +13,8 @@ import numpy as np
 import tensorflow_datasets as tfds
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+for i in range(len(physical_devices)):
+	tf.config.experimental.set_memory_growth(physical_devices[i], True)
 import math
 import multiprocessing
 
