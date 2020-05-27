aa180a57c71e3f08ced07ed654a1ef6594afb089
cody <cjb92@txstate.edu>
2020-05-26 21:09:12 -0500

    inside train

--- vgg_compression_parallel.py
+++ vgg_compression_parallel.py
@@ -330,108 +330,107 @@ def train_layer(target, pid):
 	Returns:
 		target: Updated dictonary
 	"""
+	with tf.device(f'/GPU:{pid}'):
+		dataset, info = tfds.load('cifar10', with_info=True)
 
-	dataset, info = tfds.load('cifar10', with_info=True)
+		train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+		train_dataset = train.shuffle(buffer_size=1000).batch(global_batch_size).repeat()
+		train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-	train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-	train_dataset = train.shuffle(buffer_size=1000).batch(global_batch_size).repeat()
-	train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+		test_dataset = dataset['test'].map(load_image_test, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+		test_dataset = test_dataset.batch(global_batch_size).repeat()
+		test_dataset = test_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-	test_dataset = dataset['test'].map(load_image_test, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-	test_dataset = test_dataset.batch(global_batch_size).repeat()
-	test_dataset = test_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-
-	writer = tf.summary.create_file_writer(f"./summarys/vgg/cifar10_parallel/{target['name']}")
-	with writer.as_default():
-		print(f"training layer {target['name']}")
-		tf.keras.backend.clear_session()
-		print("cleared backend")
-		model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
-		print("model loaded")
-		in_layer = target['layer']
-		get_output = tf.keras.Model(inputs=model.input, outputs=[model.layers[in_layer - 1].output,
-																model.layers[in_layer].output])
-
-
-		replacement_layers = build_replacement(get_output, layers=2)
-		replacement_len = len(replacement_layers.layers)
-		layer_train_gen = LayerBatch(get_output, train_dataset)
-		layer_test_gen = LayerTest(get_output, test_dataset)
-
-
-
-
-		MSE = tf.losses.MeanSquaredError()
-
-		optimizer=tf.keras.optimizers.SGD(.00001, momentum=.9, nesterov=True)
-		replacement_layers.compile(loss=MSE, optimizer=optimizer)
-
-		reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(patience=5, min_lr=.0001, factor=.3, verbose=1)
-		early_stop = tf.keras.callbacks.EarlyStopping(patience=15, min_delta=.0001, restore_best_weights=True, verbose=1)
-
-		replacement_layers.save(f'/tmp/layer_{pid}.h5')
-
-		print('epochs started')
-		for epoch in range(EPOCHS + 1):
-
+		writer = tf.summary.create_file_writer(f"./summarys/vgg/cifar10_parallel/{target['name']}")
+		with writer.as_default():
+			print(f"training layer {target['name']}")
 			tf.keras.backend.clear_session()
+			print("cleared backend")
 			model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
+			print("model loaded")
 			in_layer = target['layer']
 			get_output = tf.keras.Model(inputs=model.input, outputs=[model.layers[in_layer - 1].output,
 																	model.layers[in_layer].output])
 
 
-
+			replacement_layers = build_replacement(get_output, layers=2)
+			replacement_len = len(replacement_layers.layers)
 			layer_train_gen = LayerBatch(get_output, train_dataset)
 			layer_test_gen = LayerTest(get_output, test_dataset)
 
-			replacement_layers = tf.keras.models.load_model(f'/tmp/layer_{pid}.h5')
 
-			print('training started')
-			history = replacement_layers.fit(x=layer_train_gen,
-										epochs=1,
-										steps_per_epoch=TRAIN_SIZE // global_batch_size // TEST,
-										validation_data=layer_test_gen,
-										shuffle=False,
-										callbacks=[reduce_lr, early_stop],
-										validation_steps=VALIDATION_SIZE // global_batch_size // TEST,
-										verbose=1)
+
+
+			MSE = tf.losses.MeanSquaredError()
+
+			optimizer=tf.keras.optimizers.SGD(.00001, momentum=.9, nesterov=True)
+			replacement_layers.compile(loss=MSE, optimizer=optimizer)
+
+			reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(patience=5, min_lr=.0001, factor=.3, verbose=1)
+			early_stop = tf.keras.callbacks.EarlyStopping(patience=15, min_delta=.0001, restore_best_weights=True, verbose=1)
 
 			replacement_layers.save(f'/tmp/layer_{pid}.h5')
 
-			target['weights'] = [replacement_layers.layers[1].get_weights(), replacement_layers.layers[3].get_weights()]
+			print('epochs started')
+			for epoch in range(EPOCHS + 1):
 
-			tf.keras.backend.clear_session()
+				tf.keras.backend.clear_session()
+				model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
+				in_layer = target['layer']
+				get_output = tf.keras.Model(inputs=model.input, outputs=[model.layers[in_layer - 1].output,
+																		model.layers[in_layer].output])
 
-			model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
-			layer_name = target['name']
-			layer_pos = target['layer']
-			filters = model.layers[layer_pos].output.shape[-1]
 
-			new_model = replace_layer(model, layer_name, lambda x: replac(x, filters))
-			new_model.layers[layer_pos].set_weights(target['weights'][0])
-			new_model.layers[layer_pos + 2].set_weights(target['weights'][1])
-			new_model.compile(optimizer=tf.keras.optimizers.SGD(.1), loss="categorical_crossentropy", metrics=['accuracy'])
-			target['score'] = new_model.evaluate(test_dataset, steps=VALIDATION_SIZE // global_batch_size // TEST)
 
-			tf.summary.scalar(name='rep_loss', data=history.history['loss'][0], step=epoch)
-			tf.summary.scalar(name='val_loss', data=history.history['val_loss'][0], step=epoch)
-			tf.summary.scalar(name='model_acc', data=target['score'][1], step=epoch)
-			tf.summary.scalar(name='model_loss', data=target['score'][0], step=epoch)
+				layer_train_gen = LayerBatch(get_output, train_dataset)
+				layer_test_gen = LayerTest(get_output, test_dataset)
 
-			writer.flush()
-			print(f"epoch: {epoch}, rep loss {history.history['loss']}, val loss {history.history['val_loss']}, model acc {target['score'][1]}")
+				replacement_layers = tf.keras.models.load_model(f'/tmp/layer_{pid}.h5')
+
+				print('training started')
+				history = replacement_layers.fit(x=layer_train_gen,
+											epochs=1,
+											steps_per_epoch=TRAIN_SIZE // global_batch_size // TEST,
+											validation_data=layer_test_gen,
+											shuffle=False,
+											callbacks=[reduce_lr, early_stop],
+											validation_steps=VALIDATION_SIZE // global_batch_size // TEST,
+											verbose=1)
+
+				replacement_layers.save(f'/tmp/layer_{pid}.h5')
+
+				target['weights'] = [replacement_layers.layers[1].get_weights(), replacement_layers.layers[3].get_weights()]
+
+				tf.keras.backend.clear_session()
+
+				model = tf.keras.models.load_model('base_model_cifar10_vgg16.h5')
+				layer_name = target['name']
+				layer_pos = target['layer']
+				filters = model.layers[layer_pos].output.shape[-1]
+
+				new_model = replace_layer(model, layer_name, lambda x: replac(x, filters))
+				new_model.layers[layer_pos].set_weights(target['weights'][0])
+				new_model.layers[layer_pos + 2].set_weights(target['weights'][1])
+				new_model.compile(optimizer=tf.keras.optimizers.SGD(.1), loss="categorical_crossentropy", metrics=['accuracy'])
+				target['score'] = new_model.evaluate(test_dataset, steps=VALIDATION_SIZE // global_batch_size // TEST)
+
+				tf.summary.scalar(name='rep_loss', data=history.history['loss'][0], step=epoch)
+				tf.summary.scalar(name='val_loss', data=history.history['val_loss'][0], step=epoch)
+				tf.summary.scalar(name='model_acc', data=target['score'][1], step=epoch)
+				tf.summary.scalar(name='model_loss', data=target['score'][0], step=epoch)
+
+				writer.flush()
+				print(f"epoch: {epoch}, rep loss {history.history['loss']}, val loss {history.history['val_loss']}, model acc {target['score'][1]}")
 
 	return target
 
 
 def process(targets, pid, q):
 	print(f'\n\n\n\n\n PID: {pid}')
-	with tf.device(f'/GPU:{pid}'):
 
-		for i in range(pid, len(targets), NUM_PROC):
-			target = train_layer(targets[i], pid)
-			q.put(target)
+	for i in range(pid, len(targets), NUM_PROC):
+		target = train_layer(targets[i], pid)
+		q.put(target)
 
 
 
