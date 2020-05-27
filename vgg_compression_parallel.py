e4f277702d7932db15621c7492b2e2fa44c4848d
cody <cjb92@txstate.edu>
2020-05-27 13:45:21 -0500

    tabs

--- vgg_compression_parallel.py
+++ vgg_compression_parallel.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
 	tf.debugging
 	with tf.device(f'/GPU:{rank}'):
 		a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-  		b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+		b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 
 		c = tf.matmul(a, b)
 
