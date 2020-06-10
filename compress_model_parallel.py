6f5a1b45740f1bf69d6122bab82d32527c68c09c
cody <cjb92@txstate.edu>
2020-06-10 16:43:38 -0500

    fixes file path flag

--- compress_model_parallel.py
+++ compress_model_parallel.py
@@ -190,7 +190,7 @@ if __name__ == '__main__':
 						help="model architecture being compressed ex. vgg, resnet",
 						choices=['vgg', 'resnet'], default='resnet')
 	parser.add_argument("-mp", "--model_path", type=str, help="file path to saved model file", default='cifar10.h5')
-	parser.add_argument("-tp", "target_file_path", type=str, help="path to target file", default='targets_resnet.json')
+	parser.add_argument("-tp", "--target_file_path", type=str, help="path to target file", default='targets_resnet.json')
 
 	args = parser.parse_args()
 	IMAGE_SIZE = (args.image_size, args.image_size)
