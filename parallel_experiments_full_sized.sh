2ceb58f5136fb64e2b635907996592ce55bf60a2
cody <cjb92@txstate.edu>
2020-06-17 17:09:50 -0500

    finilzes script

--- parallel_experiments_full_sized.sh
+++ parallel_experiments_full_sized.sh
@@ -1,21 +1,22 @@
 mpiexec -n=4 python compress_model_parallel.py \
-						-bs=16 \
-						-im=224
+						-bs=32 \
+						-im=224 \
 						-sp=".summarys/vgg/cifar10_parallel_4_gpu/" \
 						-tp="./timing_info/vgg/four_gpu_cifar10_224.json" \
 						-ep=2 \
 						-mp="./base_model_cifar10_vgg16.h5" \
-						-tm=100 \
-						-ar='vgg' \
+						-tm=1 \
+						-ar="vgg" \
+						-sd="layer_schedules/vgg16/4gpu_opt_last.json" \
 						-tf="targets.json"
 
 
 python compress_model.py \
-			-bs=16 \
+			-bs=32 \
 			-im=224 \
 			-sp=".summarys/vgg/cifar10_224/" \
 			-tp="./timing_info/vgg/cifar10_224.json" \
 			-ep=2 \
 			-mp="./base_model_cifar10_vgg16.h5" \
-			-tm=100 \
-			-ar='vgg' \
+			-tm=1 \
+			-ar="vgg"
