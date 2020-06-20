2bec5367010ed82d8e123382c58a2dd288e8dcd1
cody <cjb92@txstate.edu>
2020-06-19 19:11:13 -0500

    bs

--- resnet_cache.sh
+++ resnet_cache.sh
@@ -1,12 +1,12 @@
 mpiexec -n=4 python compress_model_parallel.py \
-						-bs=128 \
+						-bs=256 \
 						-im=64 \
 						-sp=".summarys/resnet/cifar10_parallel_4_gpu_cache/" \
 						-tp="resnet_four_gpu_cifar10_cache.json" \
 						-ep=30
 
 python compress_model.py \
-			-bs=128 \
+			-bs=256 \
 			-im=64 \
 			-sp=".summarys/resnet/cifar10_cache/" \
 			-tp="resnet_cifar10_cache.json" \
