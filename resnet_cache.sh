ed670da2617cfee78253609abe8e1ead497eea0f
cody <cjb92@txstate.edu>
2020-06-21 11:04:27 -0500

    change script

--- resnet_cache.sh
+++ resnet_cache.sh
@@ -1,14 +1,14 @@
 mpiexec -n=4 python compress_model_parallel.py \
 						-bs=256 \
 						-im=64 \
-						-sp=".summarys/resnet/cifar10_parallel_4_gpu_cache/" \
-						-tp="resnet_four_gpu_cifar10_cache.json" \
+						-sp=".summarys/resnet/cifar10_parallel_4_gpu_fixed_calls/" \
+						-tp="resnet_four_gpu_cifar10_fixed_calls.json" \
 						-ep=30
 
 python compress_model.py \
 			-bs=256 \
 			-im=64 \
-			-sp=".summarys/resnet/cifar10_cache/" \
-			-tp="resnet_cifar10_cache.json" \
+			-sp=".summarys/resnet/cifar10_fixed_calls/" \
+			-tp="resnet_cifar10_fixed_calls.json" \
 			-ep=30
 
