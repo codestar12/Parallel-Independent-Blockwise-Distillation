cf02514f3f98beef00dfb2eb2a5a49d2e1b1d09f
cody <cjb92@txstate.edu>
2020-06-21 11:09:39 -0500

    remove interop prallelism

--- resnet_cache.sh
+++ resnet_cache.sh
@@ -12,3 +12,18 @@ python compress_model.py \
 			-tp="resnet_cifar10_fixed_calls.json" \
 			-ep=30
 
+mpiexec -n=4 python compress_model_parallel.py \
+						-bs=256 \
+						-im=64 \
+						-sp=".summarys/resnet/cifar10_parallel_4_gpu_fixed_calls_cache/" \
+						-tp="resnet_four_gpu_cifar10_fixed_calls_cache.json" \
+						-ep=30 \ 
+						-aug=False
+
+python compress_model.py \
+			-bs=256 \
+			-im=64 \
+			-sp=".summarys/resnet/cifar10_fixed_calls_cache/" \
+			-tp="resnet_cifar10_fixed_calls_cache.json" \
+			-ep=30 \
+			-aug=False
