579dca3ccf7d9892937c68ac6ad6a4c207bc014e
cody <cjb92@txstate.edu>
2020-06-24 11:56:42 -0500

    add data

--- resnet_cache.sh
+++ resnet_cache.sh
@@ -5,6 +5,20 @@ mpiexec -n=4 python compress_model_parallel.py \
 						-tp="resnet_four_gpu_cifar10_fixed_calls.json" \
 						-ep=30
 
+mpiexec -n=3 python compress_model_parallel.py \
+						-bs=256 \
+						-im=64 \
+						-sp=".summarys/resnet/cifar10_parallel_3_gpu_fixed_calls/" \
+						-tp="resnet_three_gpu_cifar10_fixed_calls.json" \
+						-ep=30
+
+mpiexec -n=2 python compress_model_parallel.py \
+						-bs=256 \
+						-im=64 \
+						-sp=".summarys/resnet/cifar10_parallel_2_gpu_fixed_calls/" \
+						-tp="resnet_two_gpu_cifar10_fixed_calls.json" \
+						-ep=30
+
 python compress_model.py \
 			-bs=256 \
 			-im=64 \
@@ -17,7 +31,7 @@ mpiexec -n=4 python compress_model_parallel.py \
 						-im=64 \
 						-sp=".summarys/resnet/cifar10_parallel_4_gpu_fixed_calls_cache/" \
 						-tp="resnet_four_gpu_cifar10_fixed_calls_cache.json" \
-						-ep=30 \ 
+						-ep=30 \
 						-aug=False
 
 python compress_model.py \
--- resnet_smaller_bs.sh
+++ resnet_smaller_bs.sh
@@ -1,14 +1,16 @@
 mpiexec -n=4 python compress_model_parallel.py \
 						-bs=128 \
 						-im=64 \
-						-sp=".summarys/resnet/cifar10_lrsch_parallel_4_gpu_bs_64/" \
-						-tp="resnet_four_gpu_cifar10_lrsch_bs_64.json" \
-						-ep=30
+						-sp=".summarys/resnet/cifar10_lrsch_parallel_4_gpu_bs_128_no_aug/" \
+						-tp="resnet_four_gpu_cifar10_lrsch_bs_128_no_aug.json" \
+						-ep=30 \
+						-aug=False
 
 python compress_model.py \
 			-bs=128 \
 			-im=64 \
-			-sp=".summarys/resnet/cifar10_lrsch_bs_64/" \
-			-tp="resnet_cifar10_lrsch_bs_64.json" \
-			-ep=30
+			-sp=".summarys/resnet/cifar10_lrsch_bs_128_no_aug/" \
+			-tp="resnet_cifar10_lrsch_bs_128_no_aug.json" \
+			-ep=30 \
+			-aug=False
 
