ff3279af3bdf2d52e0b05ab8e909b026cebc426c
cody <cjb92@txstate.edu>
2020-07-07 14:08:14 -0500

    remove unneeded file

--- scripts/resnet_cache.sh
+++ scripts/resnet_cache.sh
@@ -1,7 +1,7 @@
-mpiexec -n=4 python compress_model_parallel.py \
+mpiexec -n=4 python ../compress_model_parallel.py \
 						-bs=256 \
 						-im=64 \
-						-sp=".summarys/resnet/cifar10_4_epochs/" \
+						-sp="../summarys/resnet/cifar10_4_epochs/" \
 						-tp="resnet_four_gpu_epochs.json" \
 						-ep=40
 
